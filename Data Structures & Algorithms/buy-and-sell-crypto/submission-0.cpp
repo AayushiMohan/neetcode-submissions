ead7a2e5e417f968e556304da963aa9e2bc89e34
ByteSpace <aayushimohan118@gmail.com>
2026-07-24 15:59:52 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int left = 0;   // Buy day
+        int right = 1;  // Sell day
+
+        int maxProfit = 0;
+
+        while(right < prices.size()){
+
+            if(prices[right] > prices[left]){
+
+                int profit = prices[right] - prices[left];
+                maxProfit = max(maxProfit, profit);
+            }
+            else{
+                left = right;
+            }
+
+            right++;
+        }
+
+        return maxProfit;
+    }
+};
