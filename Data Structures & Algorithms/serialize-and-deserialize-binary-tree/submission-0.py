801f137e9aa513e26307df7aa9ac2563f7fc8b73
ByteSpace <aayushimohan118@gmail.com>
2026-09-16 16:08:34 +0530

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.py
@@ -0,0 +1,36 @@
+class Codec:
+
+    def serialize(self, root):
+        result = []
+
+        def dfs(node):
+            if node is None:
+                result.append("N")
+                return
+
+            result.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+
+        return ",".join(result)
+
+    def deserialize(self, data):
+        values = data.split(",")
+        self.index = 0
+
+        def dfs():
+            if values[self.index] == "N":
+                self.index += 1
+                return None
+
+            node = TreeNode(int(values[self.index]))
+            self.index += 1
+
+            node.left = dfs()
+            node.right = dfs()
+
+            return node
+
+        return dfs()
